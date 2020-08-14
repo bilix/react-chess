15fe6ad2f60d4bdc69918f90a6f5c82f307f81ed
dani <daniel.israel@tiendeo.com>
2020-08-14 19:47:09 +0200

    Updates

---
+++ precache-manifest.1d057f3228811731bb52be191ed057eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255f4499e4add1cb880d4ef8268ab53d",
+    "url": "/react-chess/index.html"
+  },
+  {
+    "revision": "7d25b7f16e6016f460fb",
+    "url": "/react-chess/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "71dad44e794e94f8b916",
+    "url": "/react-chess/static/js/2.7e63ecaf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-chess/static/js/2.7e63ecaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d25b7f16e6016f460fb",
+    "url": "/react-chess/static/js/main.2140a750.chunk.js"
+  },
+  {
+    "revision": "37d20c450dfb0a61fbab",
+    "url": "/react-chess/static/js/runtime-main.0a4b75c7.js"
+  }
+]);
